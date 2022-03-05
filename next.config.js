34c513d91244ab2fca6bd8fb60fe2e5d7f3175ec
Udbasili <33573587+udabasili@users.noreply.github.com>
2022-03-05 10:16:50 +0100

    replaced axios with fetch

--- next.config.js
+++ next.config.js
@@ -1,20 +1,8 @@
 const { withPlaiceholder } = require("@plaiceholder/next");
 
+
 module.exports = withPlaiceholder({ 
   images: {
     domains: ["images.unsplash.com", 'i.ibb.co', 'wpbingosite.com', 'www.chicintuition.com'],
   },
-   async headers() {
-    return [
-      {
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-        ]
-      }
-    ]
-  }
 });
