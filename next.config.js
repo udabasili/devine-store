69573af71123bc5b692828b9e51736d4bd366687
Udbasili <33573587+udabasili@users.noreply.github.com>
2022-03-05 08:05:39 +0100

    added cors

--- next.config.js
+++ next.config.js
@@ -1,22 +1,43 @@
 const { withPlaiceholder } = require("@plaiceholder/next");
 
 
-module.exports = withPlaiceholder({ 
-  images: {
-    domains: ["images.unsplash.com", 'i.ibb.co', 'wpbingosite.com', 'www.chicintuition.com'],
-  },
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-        ]
-      }
-    ]
+// module.exports = withPlaiceholder({ 
+//   images: {
+//     domains: ["images.unsplash.com", 'i.ibb.co', 'wpbingosite.com', 'www.chicintuition.com'],
+//   },
+//    async headers() {
+//     return [
+//       {
+//         // matching all API routes
+//         source: "/api/:path*",
+//         headers: [
+//           { key: "Access-Control-Allow-Credentials", value: "true" },
+//           { key: "Access-Control-Allow-Origin", value: "*" },
+//           { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+//           { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+//         ]
+//       }
+//     ]
+//   }
+// });
+
+module.exports = { 
+    images: {
+      domains: ["images.unsplash.com", 'i.ibb.co', 'wpbingosite.com', 'www.chicintuition.com'],
+    },
+     async headers() {
+      return [
+        {
+          // matching all API routes
+          source: "/api/:path*",
+          headers: [
+            { key: "Access-Control-Allow-Credentials", value: "true" },
+            { key: "Access-Control-Allow-Origin", value: "*" },
+            { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+            { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+          ]
+        }
+      ]
+    }
   }
-});
+  
