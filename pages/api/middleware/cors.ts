a9c65d1057b01181a3cedcec9456a4c3af7f21e2
Udbasili <33573587+udabasili@users.noreply.github.com>
2022-03-05 07:25:01 +0100

    added cors

---
+++ pages/api/middleware/cors.ts
@@ -0,0 +1,31 @@
+import Cors from 'cors'
+import { NextApiRequest, NextApiResponse } from 'next'
+
+function initMiddleware(middleware: { (req: Cors.CorsRequest, res: { statusCode?: number | undefined; setHeader(key: string, value: string): any; end(): any }, next: (err?: any) => any): void; (arg0: any, arg1: any, arg2: (result: any) => void): void }) {
+    return (req: any, res: any) =>
+      new Promise((resolve, reject) => {
+        middleware(req, res, (result: unknown) => {
+          if (result instanceof Error) {
+            return reject(result)
+          }
+          return resolve(result)
+        })
+      })
+}
+// Initialize the cors middleware
+const cors = initMiddleware(
+  // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+  Cors({
+    origin: ["devine-store.vercel.app" , "devine-store-git-main-udabasili.vercel.app", "devine-store-udabasili.vercel.app"],
+    // Only allow requests with GET, POST and OPTIONS
+    methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'DELETE'],
+  })
+)
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  // Run cors
+  await cors(req, res)
+
+  // Rest of the API logic
+  res.json({ message: 'Hello Everyone!' })
+}
