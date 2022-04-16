25edfa42539d9deab9e0b90f71d71ea4789b7969
Udbasili <33573587+udabasili@users.noreply.github.com>
2022-04-16 07:45:00 +0100

    updated data

--- data/slideData.ts
+++ data/slideData.ts
@@ -7,13 +7,13 @@ export const jewelryData = [
     {
         title: 'Jade Collection',
         description: 'Jade is a sought after material',
-        image: 'https://images.unsplash.com/photo-1617038220319-276d3cfab638?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80', 
+        image: 'https://images.unsplash.com/photo-1599643478518-a784e5dc4c8f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80', 
 
     },
     {
         title: 'Mali Necklace',
         description: 'Gold Country Map Of Mali Necklace Mli Pendant Chain Tuareg Jewelry',
-        image: 'https://images.unsplash.com/photo-1596944924616-7b38e7cfac36?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80', 
+        image: 'https://images.unsplash.com/photo-1599643478518-a784e5dc4c8f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80', 
 
     }
 ]
